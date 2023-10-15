e3653ec90ad98368ef20197d46a72cd8793e860e
IampatelAman <amanpatel2001@gmail.com>
2023-10-15 13:07:54 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement(
+    "div",
+    {id:"child"},
+    [React.createElement("h1",{},"I am a H1"),React.createElement("h2",{},"I am a H2")]
+)
+
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id:"heading", xyz: "abc"},
+//      "Hello world from React "
+//      );
+
+     console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
